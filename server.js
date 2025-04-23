e6dfeabbe06c354d65ec23e910f873af546204f7
Elhassan_dahmouchi <hassandahmouchi0@gmail.com>
2025-04-23 12:28:13 +0100

    deploy

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
-const { Server } = require("socket.io");
-const http = require("http");
+// server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
 
-const express = require("express");
 const app = express();
-
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // Replace with your deployed frontend
-    methods: ["GET", "POST"]
-  },
+    origin: [
+        "http://localhost:3000",              // Local frontend
+        "https://alerte-project.vercel.app"    // Production frontend
+      ],// allow from all origins or specify your Next.js domain
+    methods: ['GET', 'POST']
+  }
 });
 
+app.use(cors());
+
 io.on('connection', (socket) => {
-  console.log("User connected!");
+  console.log('A user connected:', socket.id);
 
   socket.on("notifyUser", () => {
-    io.emit("notifyUser", "Notification From Admin");
+    // Broadcast a 'notifyUser' event to all connected clients with a message
+         io.emit("notifyUser", "Notification From Admin")
+     })
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
   });
 });
 
-const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+server.listen(3001, () => {
+  console.log('Socket.IO server is running on port 3001');
 });
